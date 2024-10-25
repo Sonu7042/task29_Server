730197e16b5033302d146d72f4414bc92626ced0
SonuSingh05 <sonusinghmourya143@gmail.com>
2024-10-25 18:02:29 +0530

    task29_Sever

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const cors= require('cors')
+const fs= require('fs')
+
+const app = express();
+app.use(cors())
+
+
+app.get("/video1", (req, res) => {
+ const video1Path= path.join(__dirname, "./Public", "video1.mp4")
+ const video1Stream = fs.createReadStream(video1Path);
+ res.writeHead(200, {"Content-Type":"video/mp4"})
+
+ video1Stream.pipe(res)
+});
+
+
+
+app.get("/video2", (req, res) => {
+  const video1Path= path.join(__dirname, "./Public", "movie.mp4")
+  const video1Stream = fs.createReadStream(video1Path);
+  res.writeHead(200, {"Content-Type":"video/mp4"})
+ 
+  video1Stream.pipe(res)
+ });
+ 
+
+
+
+
+
+
+app.listen(9000, () => {
+  console.log("Server is running on port 9000");
+});
